222adae24cedffd16c1095c1b4d9c3774e2381f9
Shchukin <49319535+Fremays@users.noreply.github.com>
2023-04-14 13:22:19 +0300

    Fix to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,21 @@
 import { Inter } from 'next/font/google'
 import Header from "@/components/Header/Header";
 import Head from 'next/head';
-import Subscription from '@/components/Subscription/Subscription';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-<<<<<<< HEAD
-      <Head></Head>
-      <Header />
-      <Subscription />
-=======
-      <Head>
-        <title>Главная</title>
-        <meta
-          name="description"
-          content="Смотреть фильмы онлайн в хорошем качестве"
-        />
-      </Head>
-      {/* optimum subscription */}
-      {/* ... */}
->>>>>>> 64d2f40f0c42cece9801d6c51ebdaba1ab4a227a
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Главная</title>
+                <meta
+                    name="description"
+                    content="Смотреть фильмы онлайн в хорошем качестве"
+                />
+            </Head>
+            {/* optimum subscription */}
+            {/* ... */}
+        </>
+    )
 }
